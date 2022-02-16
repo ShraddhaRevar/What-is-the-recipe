22fa630f3dc04b3b85b4267082fc46019cd6be72
varunvjr <varun.vjr@gmail.com>
2022-02-16 12:18:26 -0800

    Database connection has been setup

--- food-recipe-server/app.js
+++ food-recipe-server/app.js
@@ -1,6 +1,9 @@
 const express=require("express");
 const app=express();
 const PORT=5000;
+require("dotenv").config();
+const connectDB=require("./config/db");
+connectDB();
 
 app.get("/",(req,res)=>{
     res.send("Food Recipe App Server");
