ed666859acd963fbd7d2794bbd1e9cc64e8d5f54
ShraddhaRevar <srevar@pdx.edu>
2022-02-17 19:14:51 -0800

    tag removed

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h2 className="App-title">Recipe Search</h2>
+          
         </header>
         <SearchBar onSubmit={this.searchRecipe}/>
         <Recipes recipes={this.state.recipes} />
