5b97cecd7590daf8703af2190b7487470b733726
varunvjr <varun.vjr@gmail.com>
2022-02-17 22:17:02 -0800

    Schema designed for food and recipe

--- food-recipe-server/app.js
+++ food-recipe-server/app.js
@@ -1,10 +1,11 @@
 const express=require("express");
 const app=express();
 const PORT=5000;
+const User=require("./models/user");
+const Recipe=require("./models/foodRecipe");
 require("dotenv").config();
 const connectDB=require("./config/db");
 connectDB();
-
 app.get("/",(req,res)=>{
     res.send("Food Recipe App Server");
 })
