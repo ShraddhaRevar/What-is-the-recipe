0323f536aea68b575ffef57aa0074ac5d3a3fb92
varunvjr <varun.vjr@gmail.com>
2022-02-24 13:47:57 -0800

    Food Recipe server setup

--- food-recipe-server/app.js
+++ food-recipe-server/app.js
@@ -1,12 +1,14 @@
 const express=require("express");
 const app=express();
 const PORT=5000;
+const cors=require("cors");
 
 const userRouter=require("./routes/userRouter");
 const foodRouter=require("./routes/foodRouter"); 
 require("dotenv").config();
 const connectDB=require("./config/db");
 connectDB();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/users",userRouter);
