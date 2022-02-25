0d0437ad14a1efc182753a775d60ce2890fdd5c8
varunvjr <varun.vjr@gmail.com>
2022-02-25 12:57:43 -0800

    Image added in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react'
-
+import React from 'react';
+import RecipeHome from "./images/RecipeHome.jpg";
 
 import Header from './components/Header';
 
 const App = () => {
   return (
+    <>
+    <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link
+      href="https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css"
+      rel="stylesheet"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap"
+      rel="stylesheet"
+    />
+      </head>
     <div className='App'>
       <Header/>
+    
     </div>
+    <img style={{
+      width:"100%",
+      height:"100%"
+    }} src={RecipeHome} alt="recipeHome.jpeg"/>
+    </>
+    
   
    
   )
