70d0d52ef3a7f385786b68eefcced4fb1c9a1942
ShraddhaRevar <srevar@pdx.edu>
2022-03-10 15:06:43 -0800

    Updates

---
+++ precache-manifest.83f5e2b3866a5ed4e6c6263d9c113748.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e3e7d30ed0127e23d59859eb8756083",
+    "url": "/What-is-the-recipe/index.html"
+  },
+  {
+    "revision": "6f5d0518e0519993b606",
+    "url": "/What-is-the-recipe/static/css/2.78eeb89d.chunk.css"
+  },
+  {
+    "revision": "9aed7dca8b26cbaf2af2",
+    "url": "/What-is-the-recipe/static/css/main.19fec7c1.chunk.css"
+  },
+  {
+    "revision": "6f5d0518e0519993b606",
+    "url": "/What-is-the-recipe/static/js/2.4076b348.chunk.js"
+  },
+  {
+    "revision": "9aed7dca8b26cbaf2af2",
+    "url": "/What-is-the-recipe/static/js/main.221ec924.chunk.js"
+  },
+  {
+    "revision": "7ece6dfeae154d033566",
+    "url": "/What-is-the-recipe/static/js/runtime~main.8232d03b.js"
+  },
+  {
+    "revision": "ff9dc3f3b8bc931577063217feec3d79",
+    "url": "/What-is-the-recipe/static/media/RecipeHome.ff9dc3f3.jpg"
+  }
+]);
